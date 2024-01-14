09eb10814a7de2fabd87219482efb1a505789d9c
serega <derevchuk.s@gmail.com>
2024-01-14 09:02:58 +0300

    Добавление отдельного файла с Secret_Key

--- Shop/settings.py
+++ Shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
  
 from pathlib import Path
+from secret import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@l^iz2ouh1n3w(@lt4@0lxphj=em88ad_$uje$9dh+g6zqr2w6'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
