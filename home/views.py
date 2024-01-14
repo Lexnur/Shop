fb3dc1318ab9e7da2a853bc78590afad4d441d11
serega <derevchuk.s@gmail.com>
2024-01-14 15:53:19 +0300

    Добавление во views.py

--- home/views.py
+++ home/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def info(request):
-    return HttpResponse('Hello! Yuo are best!!!')
+    return HttpResponse("Hello! If you reading this - don't worry, "
+                        "this sait is under development and adding information")
